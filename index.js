eb38ba5c120abee40459e8fce7c80e4c9519ab5a
Jesper Pedersen <jeppenp12@gmail.com>
2021-11-09 02:42:17 +0100

    VALORANT leaderboard

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const RIOT_TOKEN = process.env.RIOT_TOKEN;
 bot.login(DISCORD_TOKEN);
 
 // Stored Discord variables
-let channel = "906284346762215424";
+let league_channel = "906284346762215424";
+let valorant_channel = "907393865475043378";
 let id = '';
 
 // Firebase APIs
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore');
-const { sign } = require('crypto');
 
 const firebaseConfig = {
     apiKey: process.env.apiKey,
@@ -36,20 +36,31 @@ bot.on('ready', () => {
     bot.user.setActivity('ur rank kekw', {
         type: 'WATCHING'
     });
-    getSummonerID(db);
+    // getSummonerID(db);
+    getValorantPlayers(db);
 });
 
 
 // Fetch Summoner Data from Firebase
 async function getSummonerID(db) {
+    // League of Legends
     const summoners = collection(db, 'summoners');
     const summonerSnapshot = await getDocs(summoners);
     const summonerList = summonerSnapshot.docs.map(doc => doc.data());
-    ListRanks(summonerList);
+    ListLeagueRanks(summonerList);
+}
+
+// Fetch Valorant Player Data from Firebase
+async function getValorantPlayers(db) {
+    // VALORANT
+    const players = collection(db, 'valorant');
+    const playerSnapshot = await getDocs(players);
+    const playerList = playerSnapshot.docs.map(doc => doc.data());
+    ListValorantRanks(playerList);
 }
 
 // Do API call to Riot for every Summoner 
-function ListRanks(summonerList) {
+function ListLeagueRanks(summonerList) {
     let region = "euw1.api.riotgames.com"
     var summonerObj = [];
 
@@ -72,11 +83,70 @@ function ListRanks(summonerList) {
 
     // When we finish constructing ranks
     listRanks.then((summonerObj) => {
-        constructEmbed(summonerObj)
+        constructLeagueEmbed(summonerObj)
     })
 }
 
-// Assemble Data Object
+// Do API call to Riot for every Summoner 
+function ListValorantRanks(playerList) {
+    let region = "api.henrikdev.xyz"
+    var playerObj = [];
+
+    var listRanks = new Promise((resolve, reject) => {
+        playerList.forEach((player, index, array) => {
+            let route = 'https://' + region + '/valorant/v2/mmr/eu/' + player.playername + "/" + player.playertag;
+            let itemArray = {};
+            fetch(route)
+            .then(res => 
+                res.json()
+            )
+            .then(json => 
+                assembleValorantData(playerObj, json.data)
+            )
+            .then((playerObj) => {
+                if(Object.keys(playerObj).length === playerList.length) resolve(playerObj)
+            })
+        })
+    });
+
+    // When we finish constructing ranks
+    listRanks.then((playerObj) => {
+        constructValorantEmbed(playerObj)
+    })
+}
+
+
+// Assemble Data Object for League of Legends
+function assembleValorantData(playerObj, player) {
+    /* Player Object has
+    -- 
+        name
+        tag
+        current_data {
+            currenttier
+            currenttierpatched
+            ranking_in_tier
+            mmr_change_to_last_game
+            elo
+            games_needed_for_rating
+        }
+    */
+
+    var itemArray = {
+        name: player.name,
+        tag: player.tag,
+        placement: player.current_data.currenttier + (player.current_data.ranking_in_tier / 100),
+        rank: player.current_data.currenttierpatched,
+        lp: player.current_data.ranking_in_tier
+    }
+
+    playerObj.push(itemArray);
+
+    return playerObj;
+}
+
+
+// Assemble Data Object for League of Legends
 function assembleSummonerData(summonerObj, summoner) {
     /* Summoner Object has
     -- 
@@ -157,8 +227,8 @@ function assembleSummonerData(summonerObj, summoner) {
 }
 
 
-// Discord Embed
-function constructEmbed(summonerObj) {
+// Discord Embed for League of Legends
+function constructLeagueEmbed(summonerObj) {
     summonerObj.sort(function(a, b) {
         return a.placement - b.placement
     });
@@ -176,27 +246,78 @@ function constructEmbed(summonerObj) {
         playerRank += player.tier + " " + player.rank + " LP: " + player.lp + " - Wins: " + player.wins + "\n";
     })
 
-    const message = new MessageEmbed()
-    .setAuthor('Otaku Leaderboard for Ranked League', 'https://icon-library.com/images/league-of-legends-icon-png/league-of-legends-icon-png-20.jpg')
-    .setColor("#FFFFFF")
+    const embed = new MessageEmbed()
+    .setAuthor('Leaderboard for Otaku', 'https://i.imgur.com/PnuAAMj.png')
+    .setColor("#0bc6e3")
+    .setDescription("These are the current placements for participants of the League Leaderboard")
 	.addFields(
         { name: '\u200B', value: playerPlacement, inline: true},
 		{ name: '\u200B', value: playerName, inline: true },
 		{ name: '\u200B', value: playerRank, inline: true },
 	)
+    .setTimestamp()
+    .setFooter('Last updated')
     
     if(id === '') {
-        bot.channels.cache.get(channel).bulkDelete(100).then(
-            bot.channels.cache.get(channel).send({ embeds: [message] }).then(sent => {
+        bot.channels.cache.get(league_channel).bulkDelete(100).then(
+            bot.channels.cache.get(league_channel).send({ embeds: [embed] }).then(sent => {
                 id = sent.id;
-                setTimeout(function(id) { 
+                setTimeout(function() { 
                     getSummonerID(db); 
-                }, 3000);
+                }, 3600000);
             })
         )
     }
     // Edit already set message here
     else {
-        console.log("Hi Jes");
+        messageEdit = bot.channels.cache.get(league_channel).messages.fetch(id)
+        .then(message => message.edit({ embeds: [embed] }))
+        .catch(console.error);
+    }
+}
+
+// Discord Embed for League of Legends
+function constructValorantEmbed(playerObj) {
+    let i = 0;
+
+    playerObj.sort(function(a, b) {
+        return a.placement + b.placement
+    });
+    
+    let playerName = "";
+    let playerRank = "";
+    
+    playerObj.forEach((player) => {
+        i++;
+        playerName += i.toString() + ". " + player.name + "\n";
+        playerRank += "**" + player.rank + "** - Rank Progress: " + player.lp + "%" + "\n";
+    });
+
+    const embed = new MessageEmbed()
+    .setAuthor('VALORANT Leaderboard', 'https://i.imgur.com/qONRQmq.png')
+    .setColor("#0bc6e3")
+    .setDescription("These are the current placements for participants of the Otaku Leaderboard. The Leaderboard is updated every hour or by manual restart.")
+	.addFields(
+		{ name: 'Name', value: playerName, inline: true },
+		{ name: 'Rank', value: playerRank, inline: true },
+	)
+    .setTimestamp()
+    .setFooter('Last updated')
+    
+    if(id === '') {
+        bot.channels.cache.get(valorant_channel).bulkDelete(100).then(
+            bot.channels.cache.get(valorant_channel).send({ embeds: [embed] }).then(sent => {
+                id = sent.id;
+                setTimeout(function() { 
+                    getValorantPlayers(db); 
+                }, 3600000);
+            })
+        )
+    }
+    // Edit already set message here
+    else {
+        messageEdit = bot.channels.cache.get(valorant_channel).messages.fetch(id)
+        .then(message => message.edit({ embeds: [embed] }))
+        .catch(console.error);
     }
 }
